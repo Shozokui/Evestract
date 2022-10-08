ea4fb985d26bd666f821e7044d91f7047f80ede0
Velyn <46884288+mehvvy@users.noreply.github.com>
2022-10-08 04:11:57 -0700

    Update scheduler parsing

--- src/parser/parsers/scheduler.c
+++ src/parser/parsers/scheduler.c
@@ -23,6 +23,54 @@ static void getFourCC(const uint8_t* buf, char* out) {
 
 typedef void (*disSchedInst)(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint32_t iLen0, uint32_t iLen1);
 
+static void disEnd(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint32_t iLen0, uint32_t iLen1) {
+    (void) buf;
+    (void) inst;
+    (void) instLen;
+
+    if (instLen != 1) {
+        printf("\t\tWARNING: OpCode Length %u unexpected!\n", instLen);
+    }
+
+    if (iLen0 != 0 || iLen1 != 0) {
+        printf("\t\tEND(%d, %d);\n", iLen0, iLen1);
+    } else {
+        printf("\t\tEND();\n");
+    }
+}
+
+static void disIdleEnd(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint32_t iLen0, uint32_t iLen1) {
+    (void) buf;
+    (void) inst;
+    (void) instLen;
+
+    if (instLen != 2) {
+        printf("\t\tWARNING: OpCode Length %u unexpected!\n", instLen);
+    }
+
+    if (iLen0 != 0 || iLen1 != 0) {
+        printf("\t\tEND(%d, %d);\n", iLen0, iLen1);
+    } else {
+        printf("\t\tEND();\n");
+    }
+}
+
+static void disIdleInit(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint32_t iLen0, uint32_t iLen1) {
+    (void) buf;
+    (void) inst;
+    (void) instLen;
+
+    if (instLen != 2) {
+        printf("\t\tWARNING: OpCode Length %u unexpected!\n", instLen);
+    }
+
+    if (iLen0 != 0 || iLen1 != 0) {
+        printf("\t\tINIT(%d, %d);\n", iLen0, iLen1);
+    } else {
+        printf("\t\tINIT();\n");
+    }
+}
+
 static void disSched02(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint32_t iLen0, uint32_t iLen1) {
     // 02 FourCC
     // 03 <generator ptr>
@@ -30,6 +78,10 @@ static void disSched02(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint
     (void) instLen;
 
     char fourCC[32];
+
+    if (instLen != 4) {
+        printf("\t\tWARNING: OpCode Length %u unexpected!\n", instLen);
+    }
 
     getFourCC(&buf[8], fourCC);
 
@@ -40,15 +92,135 @@ static void disSched02(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint
     }
 }
 
-static disSchedInst disSchedFunc[256] = {
-    [0x02] = disSched02,
+static void disSched03(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint32_t iLen0, uint32_t iLen1) {
+    // 02 FourCC
+    // 03 <generator ptr>
+    (void) inst;
+    (void) instLen;
+
+    char fourCC[32];
+
+    if (instLen != 4) {
+        printf("\t\tWARNING: OpCode Length %u unexpected!\n", instLen);
+    }
+
+    getFourCC(&buf[8], fourCC);
+
+    if (iLen0 != 0 || iLen1 != 0) {
+        printf("\t\t_03(%d, %d, %s);\n", iLen0, iLen1, fourCC);
+    } else {
+        printf("\t\t_03(%s);\n", fourCC);
+    }
+}
+
+static void disSched0A(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint32_t iLen0, uint32_t iLen1) {
+    // 02 FourCC
+    // 03 <generator ptr>
+    (void) inst;
+    (void) instLen;
+
+    char fourCC[32];
+
+    getFourCC(&buf[8], fourCC);
+
+    if (iLen0 != 0 || iLen1 != 0) {
+        printf("\t\t_0A(%d, %d, %s, 0x%08X, 0x%08X, 0x%08X, 0x%08X);\n", iLen0, iLen1, fourCC, lsb32(buf, 16), lsb32(buf, 20), lsb32(buf, 24), lsb32(buf, 28));
+    } else {
+        printf("\t\t_0A(%s, 0x%08X, 0x%08X, 0x%08X, 0x%08X);\n", fourCC, lsb32(buf, 16), lsb32(buf, 20), lsb32(buf, 24), lsb32(buf, 28));
+    }
+}
+
+static void disSched0B(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint32_t iLen0, uint32_t iLen1) {
+    // 02 FourCC
+    // 03 <generator ptr>
+    (void) inst;
+    (void) instLen;
+
+    char fourCC[32];
+
+    getFourCC(&buf[8], fourCC);
+
+    if (iLen0 != 0 || iLen1 != 0) {
+        printf("\t\t_0B(%d, %d, %s, 0x%08X, 0x%08X, 0x%08X, 0x%08X);\n", iLen0, iLen1, fourCC, lsb32(buf, 16), lsb32(buf, 20), lsb32(buf, 24), lsb32(buf, 28));
+    } else {
+        printf("\t\t_0B(%s, 0x%08X, 0x%08X, 0x%08X, 0x%08X);\n", fourCC, lsb32(buf, 16), lsb32(buf, 20), lsb32(buf, 24), lsb32(buf, 28));
+    }
+}
+
+static void disSched73(const uint8_t* buf, uint32_t inst, uint32_t instLen, uint32_t iLen0, uint32_t iLen1) {
+    // 02 FourCC
+    // 03 <generator ptr>
+    (void) inst;
+    (void) instLen;
+
+    char fourCC[32];
+
+    getFourCC(&buf[8], fourCC);
+
+    if (iLen0 != 0 || iLen1 != 0) {
+        printf("\t\t_73(%d, %d, %s);\n", iLen0, iLen1, fourCC);
+    } else {
+        printf("\t\t_73(%s);\n", fourCC);
+    }
+}
+
+static disSchedInst disInitFunc[256] = {
+    [0x00] = disEnd,
 };
 
-static void disSched(const uint8_t* buf) {
+static disSchedInst disIdleFunc[256] = {
+    [0x00] = disIdleEnd,
+    [0x01] = disIdleInit,
+    [0x02] = disSched02,
+    [0x03] = disSched03,
+    [0x0A] = disSched0A,
+    [0x0B] = disSched0B,
+    [0x73] = disSched73,
+};
+
+static disSchedInst disDieFunc[256] = {
+    [0x00] = disEnd,
+};
+
+static void disInit(const uint8_t* buf) {
     for (;;) {
-        uint32_t inst = buf[0];
-        uint32_t instLen = buf[1];
-        uint32_t unk0 = lsb16(buf, 0x02);
+        uint32_t v = lsb32(buf, 0);
+        uint32_t inst = v & 0xFF;
+        uint32_t instLen = (v >> 8) & 0x1F;
+        uint32_t unk0 = v >> 13;
+
+        uint32_t iLen0 = 0; //lsb16(buf, 0x04);
+        uint32_t iLen1 = 0; //lsb16(buf, 0x06);
+
+        if (unk0 != 0) {
+            printf("\t\tUnk0: %04X\n", unk0);
+        }
+
+        disSchedInst func = disInitFunc[inst];
+        if (func != NULL) {
+            func(buf, inst, instLen, iLen0, iLen1);
+        } else {
+            printf("\t\t_%02X(%d, %d", inst, iLen0, iLen1);
+            for (uint32_t i = 1; i < instLen; i++) {
+                printf(", 0x%08X", lsb32(buf, i * 4));
+            }
+            printf(");\n");
+        }
+
+        buf += (instLen * 4);
+
+        if (inst == 0) {
+            break;
+        }
+    }
+}
+
+static void disIdle(const uint8_t* buf) {
+    for (;;) {
+        uint32_t v = lsb32(buf, 0);
+        uint32_t inst = v & 0xFF;
+        uint32_t instLen = (v >> 8) & 0x1F;
+        uint32_t unk0 = v >> 13;
 
         uint32_t iLen0 = lsb16(buf, 0x04);
         uint32_t iLen1 = lsb16(buf, 0x06);
@@ -57,7 +229,7 @@ static void disSched(const uint8_t* buf) {
             printf("\t\tUnk0: %04X\n", unk0);
         }
 
-        disSchedInst func = disSchedFunc[inst];
+        disSchedInst func = disIdleFunc[inst];
         if (func != NULL) {
             func(buf, inst, instLen, iLen0, iLen1);
         } else {
@@ -76,24 +248,57 @@ static void disSched(const uint8_t* buf) {
     }
 }
 
+static void disDie(const uint8_t* buf) {
+    for (;;) {
+        uint32_t v = lsb32(buf, 0);
+        uint32_t inst = v & 0xFF;
+        uint32_t instLen = (v >> 8) & 0x1F;
+        uint32_t unk0 = v >> 13;
+
+        uint32_t iLen0 = 0; //lsb16(buf, 0x04);
+        uint32_t iLen1 = 0; //lsb16(buf, 0x06);
+
+        if (unk0 != 0) {
+            printf("\t\tUnk0: %04X\n", unk0);
+        }
+
+        disSchedInst func = disDieFunc[inst];
+        if (func != NULL) {
+            func(buf, inst, instLen, iLen0, iLen1);
+        } else {
+            printf("\t\t_%02X(%d, %d", inst, iLen0, iLen1);
+            for (uint32_t i = 1; i < instLen; i++) {
+                printf(", 0x%08X", lsb32(buf, i * 4));
+            }
+            printf(");\n");
+        }
+
+        buf += (instLen * 4);
+
+        if (inst == 0) {
+            break;
+        }
+    }
+}
+
 int parseScheduler(const chunk_t* chunk) {
     const uint8_t* buf = chunk->buf;
 
-    uint32_t aOff = lsb32(buf, 0x20);
-    uint32_t bOff = lsb32(buf, 0x24);
-    uint32_t cOff = lsb32(buf, 0x28);
+    uint32_t offInit = lsb32(buf, 0x20);
+    uint32_t offIdle = lsb32(buf, 0x24);
+    uint32_t offDie = lsb32(buf, 0x28);
     uint32_t sLen = lsb32(buf, 0x2C);
 
     printf("\tsLen: %u\n", sLen);
 
-    printf("\taOff: %u\n", aOff);
-    disSched(&buf[aOff]);
+    printf("\tInit:\n");
+    disInit(&buf[offInit]);
 
-    printf("\tbOff: %u\n", bOff);
-    disSched(&buf[bOff]);
+    printf("\tIdle:\n");
+    disIdle(&buf[offIdle]);
 
-    printf("\tcOff: %u\n", cOff);
-    disSched(&buf[cOff]);
+    printf("\tDie:\n");
+    disDie(&buf[offDie]);
 
     return 0;
 }
